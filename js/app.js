a8960c02f27e00caf39b84be970ed189826fea48
Severin Spoerri <severin.spoerri@outlook.com>
2018-10-11 17:14:10 +0200

    added basic Player Class

--- js/app.js
+++ js/app.js
@@ -25,11 +25,22 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor () {
+        this.x = 0;
+        this.y = 0;
+        this.sprite = 'images/char-boy.png';
+    }
+    render () {
+       ctx.drawImage(Resources.get(this.sprite), this.x, this.y) 
+    }
+    
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
